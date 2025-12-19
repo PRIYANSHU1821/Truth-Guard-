af5e6e249f9a5d59e3ff72b5ed7e73b247612391
fatiya17 <fatiyalabibah17@gmail.com>
2025-12-19 23:49:41 +0700

    style: add custom Tailwind color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          // background gradient colors
+          bgStart: '#81A1E0',
+          bgMid: '#C3D7F6',
+          bgEnd: '#F0F6FF',
+          // element decorative
+          star: '#FFE2DF',
+          // component colors
+          surface: '#FFFFFF',
+          text: '#000000',
+          primary: '#65A9E0',
+          secondary: '#E4EDFF',
+          accent: '#9EB7E9',
+        }
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
